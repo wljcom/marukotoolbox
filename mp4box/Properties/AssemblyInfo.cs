45fad61bbe966a3f8ffb797badc0cdf6f55527e5
Neil <neilpiken@gmail.com>
2014-06-16 08:04:00 +0800

    Update copyright dates to 2014

--- mp4box/Properties/AssemblyInfo.cs
+++ mp4box/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("mp4box")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Maruko Toolbox")]
+[assembly: AssemblyDescription("A Video-prosessing GUI")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("mp4box")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyProduct("Maruko Toolbox")]
+[assembly: AssemblyCopyright("Copyright © 2011-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
