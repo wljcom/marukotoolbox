ed7858b0580334b906d3a4d6b5951aead23a6657
Neil <neilpiken@gmail.com>
2014-10-02 21:43:00 +0800

    Add apache license information

--- mp4box/Properties/AssemblyInfo.cs
+++ mp4box/Properties/AssemblyInfo.cs
@@ -1,4 +1,22 @@
-﻿using System.Reflection;
+﻿// ------------------------------------------------------------------
+// Copyright (C) 2011-2014 Maruko Toolbox Project
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied.
+// See the License for the specific language governing permissions
+// and limitations under the License.
+// -------------------------------------------------------------------
+//
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,7 +26,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Maruko Toolbox")]
 [assembly: AssemblyDescription("A Video-prosessing GUI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("http://maruko.appinn.me/")]
 [assembly: AssemblyProduct("Maruko Toolbox")]
 [assembly: AssemblyCopyright("Copyright © 2011-2014")]
 [assembly: AssemblyTrademark("")]
