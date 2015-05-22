43fa5ae8c85d54d7f9643e8b4e37fd6390a15511
komaruchan <sandy_0308@hotmail.com>
2015-05-22 14:25:25 +0800

    Add ProgressTaskbar.cs and a new icon

---
+++ mp4box/ProgressTaskbar.cs
@@ -0,0 +1,121 @@
+﻿// ------------------------------------------------------------------
+// Copyright (C) 2011-2015 Maruko Toolbox Project
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied.
+// See the License for the specific language governing permissions
+// and limitations under the License.
+// -------------------------------------------------------------------
+//
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace mp4box
+{
+    /// <summary>
+    /// ProgressTaskbar provides Windows 7 taskbar progress
+    /// </summary>
+
+    [ComImportAttribute()]
+    [GuidAttribute("ea1afb91-9e28-4b86-90e9-9e9f8a5eefaf")]
+    [InterfaceTypeAttribute(ComInterfaceType.InterfaceIsIUnknown)]
+    interface ITaskbarList3
+    {
+        [PreserveSig]
+        void HrInit();
+        [PreserveSig]
+        void AddTab(IntPtr hwnd);
+        [PreserveSig]
+        void DeleteTab(IntPtr hwnd);
+        [PreserveSig]
+        void ActivateTab(IntPtr hwnd);
+        [PreserveSig]
+        void SetActiveAlt(IntPtr hwnd);
+
+        [PreserveSig]
+        void MarkFullscreenWindow(IntPtr hwnd, [MarshalAs(UnmanagedType.Bool)] bool fFullscreen);
+
+        void SetProgressValue(IntPtr hwnd, UInt64 ullCompleted, UInt64 ullTotal);
+        void SetProgressState(IntPtr hwnd, TBPFLAG tbpFlags);
+        void RegisterTab(IntPtr hwndTab, IntPtr hwndMDI);
+        void UnregisterTab(IntPtr hwndTab);
+        void SetTabOrder(IntPtr hwndTab, IntPtr hwndInsertBefore);
+        void SetTabActive(IntPtr hwndTab, IntPtr hwndMDI, TBATFLAG tbatFlags);
+        void ThumbBarAddButtons(IntPtr hwnd, uint cButtons, [MarshalAs(UnmanagedType.LPArray)] THUMBBUTTON[] pButtons);
+        void ThumbBarUpdateButtons(IntPtr hwnd, uint cButtons, [MarshalAs(UnmanagedType.LPArray)] THUMBBUTTON[] pButtons);
+        void ThumbBarSetImageList(IntPtr hwnd, IntPtr himl);
+        void SetOverlayIcon(IntPtr hwnd, IntPtr hIcon, [MarshalAs(UnmanagedType.LPWStr)] string pszDescription);
+        void SetThumbnailTooltip(IntPtr hwnd, [MarshalAs(UnmanagedType.LPWStr)] string pszTip);
+        void SetThumbnailClip(IntPtr hwnd, ref RECT prcClip);
+    }
+    [GuidAttribute("56FDF344-FD6D-11d0-958A-006097C9A090")]
+    [ClassInterfaceAttribute(ClassInterfaceType.None)]
+    [ComImportAttribute()]
+    class ProgressTaskbar { }
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    struct THUMBBUTTON
+    {
+        [MarshalAs(UnmanagedType.U4)]
+        public THBMASK dwMask;
+        public uint iId;
+        public uint iBitmap;
+        public IntPtr hIcon;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+        public string szTip;
+        [MarshalAs(UnmanagedType.U4)]
+        public THBFLAGS dwFlags;
+    }
+    enum THBMASK
+    {
+        THB_BITMAP = 0x1,
+        THB_ICON = 0x2,
+        THB_TOOLTIP = 0x4,
+        THB_FLAGS = 0x8
+    }
+
+    enum THBFLAGS
+    {
+        THBF_ENABLED = 0,
+        THBF_DISABLED = 0x1,
+        THBF_DISMISSONCLICK = 0x2,
+        THBF_NOBACKGROUND = 0x4,
+        THBF_HIDDEN = 0x8
+    }
+    enum TBPFLAG
+    {
+        TBPF_NOPROGRESS = 0,
+        TBPF_INDETERMINATE = 0x1,
+        TBPF_NORMAL = 0x2,
+        TBPF_ERROR = 0x4,
+        TBPF_PAUSED = 0x8
+    }
+    enum TBATFLAG
+    {
+        TBATF_USEMDITHUMBNAIL = 0x1,
+        TBATF_USEMDILIVEPREVIEW = 0x2
+    }
+    struct RECT
+    {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+
+        public RECT(int left, int top, int right, int bottom)
+        {
+            this.left = left;
+            this.top = top;
+            this.right = right;
+            this.bottom = bottom;
+        }
+    }
+}
